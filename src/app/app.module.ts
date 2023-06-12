e8fba301c886718a28df06bbfc1da691ec619789
Antonino Bonanno <nino.bonanno96@gmail.com>
2023-06-12 22:39:36 +0200

    library install

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {DesignAngularKitModule} from "design-angular-kit";
+import {TranslateStore} from '@ngx-translate/core'
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
@@ -10,9 +13,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+    DesignAngularKitModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [TranslateStore],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
