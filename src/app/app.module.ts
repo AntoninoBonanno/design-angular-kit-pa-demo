782bf79170631720985771ec5315a14a6acb7d4c
Antonino Bonanno <nino.bonanno96@gmail.com>
2023-06-12 23:44:54 +0200

    custom localization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,10 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {DesignAngularKitModule, ItLanguageSwitcherComponent, ItNotificationsComponent} from "design-angular-kit";
-import {TranslateStore} from '@ngx-translate/core'
+import {TranslateModule, TranslateLoader} from '@ngx-translate/core'
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {HttpBackend, HttpClientModule} from "@angular/common/http";
+import {MultiTranslateHttpLoader} from "ngx-translate-multi-http-loader";
 
 @NgModule({
   declarations: [
@@ -15,12 +17,25 @@ import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
     BrowserModule,
     AppRoutingModule,
 
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: (http: HttpBackend) => new MultiTranslateHttpLoader(http, [
+          './bootstrap-italia/i18n/', // Load library translations first, so you can edit the keys in your localization file
+          './assets/i18n/' // Your i18n location (You can override bootstrap-italia i18n keys)
+        ]),
+        deps: [HttpBackend]
+      },
+      defaultLanguage: 'it'
+    }),
+
     DesignAngularKitModule,
     BrowserAnimationsModule,
     ItLanguageSwitcherComponent,
     ItNotificationsComponent
   ],
-  providers: [TranslateStore],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import {ItTabModule} from "design-angular-kit";
 import {ReportTabComponent} from "../report-tab/report-tab.component";
 import {SearchTabComponent} from "../search-tab/search-tab.component";
+import {TranslateModule} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, ItTabModule, ReportTabComponent, SearchTabComponent],
+  imports: [CommonModule, ItTabModule, ReportTabComponent, SearchTabComponent, TranslateModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
