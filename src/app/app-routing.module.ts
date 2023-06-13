4fe36d9c7ccdb117fd024a2b3550608692190d79
Antonino Bonanno <nino.bonanno96@gmail.com>
2023-06-13 22:50:25 +0200

    added errors pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ItErrorPageComponent} from "design-angular-kit";
 
 const routes: Routes = [
-  {path: '', loadComponent: () => import('./components/home/home.component').then(m => m.HomeComponent)}
+  {path: '', loadComponent: () => import('./components/home/home.component').then(m => m.HomeComponent)},
+  { path: 'error/not-found', component: ItErrorPageComponent, data: { errorCode: 404 } },
+  { path: 'error/forbidden', component: ItErrorPageComponent, data: { errorCode: 403 } },
+  { path: 'error/server-error', component: ItErrorPageComponent, data: { errorCode: 500 } },
+  { path: '**', redirectTo: 'error/not-found'  }
 ];
 
 @NgModule({
